6f30594472e787c5d253cc6691ed3bac3906b96e
null <me@github0null.io>
2022-08-14 20:18:41 +0800

    update favicon.ico

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'https://em-ide.com/favicon.ico',
+  favicon: '/img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
