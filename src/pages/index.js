a9a00e0ce472e5c1e83ff597bfe8ca1fef487002
null <me@github0null.io>
2024-09-15 01:30:16 +0800

    update landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,21 +29,7 @@ export default function Home() {
                   <a class="button button-primary" href={`${localePathRoot}/docs/intro`}>Getting Start</a>
                   <a class="button" href="https://marketplace.visualstudio.com/items?itemName=CL.eide">Install</a></div>
               </div>
-              <div class="hero-figure anime-element">
-                <svg class="placeholder" width="528" height="396" viewBox="0 0 528 396">
-                  <rect width="528" height="396" style={{ fill: 'transparent' }} />
-                </svg>
-                <div class="hero-figure-box hero-figure-box-01" data-rotation="45deg"></div>
-                <div class="hero-figure-box hero-figure-box-02" data-rotation="-45deg"></div>
-                <div class="hero-figure-box hero-figure-box-03" data-rotation="0deg"></div>
-                <div class="hero-figure-box hero-figure-box-04" data-rotation="-135deg"></div>
-                <div class="hero-figure-box hero-figure-box-05"></div>
-                <div class="hero-figure-box hero-figure-box-06"></div>
-                <div class="hero-figure-box hero-figure-box-07"></div>
-                <div class="hero-figure-box hero-figure-box-08" data-rotation="-22deg"></div>
-                <div class="hero-figure-box hero-figure-box-09" data-rotation="-52deg"></div>
-                <div class="hero-figure-box hero-figure-box-10" data-rotation="-50deg"></div>
-              </div>
+              <img width="800px" src="landing_page/images/eide_preview_s.png" alt="Feature preview" />
             </div>
           </div>
         </section>
